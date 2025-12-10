73e3295cdbbd4acfe42938d022b254fb72d23335
natsukashiixo <jesper.varg.viking@gmail.com>
2025-12-10 11:29:45 +0100

    remove stray bs4 code

--- src/VideoObjects.py
+++ src/VideoObjects.py
@@ -11,8 +11,6 @@ Clip holds info about sources and other clips of the same source.
 Stream holds info about live stream specific data like clips referencing the stream, song count, song list and more. 
 Placeholder holds info about potential future streams, external streams and other events. 
 '''
-from bs4 import BeautifulSoup
-from json import loads
 import re
 from .HolodexClient import HolodexClient
 from .Misc import PlaceholderCredits, Comment
@@ -126,20 +124,6 @@ class BaseVideo:
 
     async def remove_mention(self, channel):
         return await self.remove_mentions(self.id, channel)
-
-    async def is_members(self):
-        data = await self.client.static("https://youtube.com/watch?v=" + self.id, base=False)
-        # if data.status != 200: raise
-        page = await data.text()
-        soup = BeautifulSoup(page, "html.parser")
-        j = soup.find_all("script")
-        k = (list(filter(lambda x: "playabilityStatus" in str(x), j)))
-        pattern = re.compile(r"var ytInitialPlayerResponse = (.*?);$", re.MULTILINE | re.DOTALL)
-        var = pattern.search(k[0].text)
-        raw = var.group(1)
-        final = loads(raw)
-        reason = final.get("playabilityStatus", {}).get("reason", "")
-        return "get access to members-only content" in reason
 
 class Streamable(BaseVideo):
 
